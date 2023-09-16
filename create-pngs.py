a87472bba8c1acfb4e8a622fb8fdeffb159a0dd6
the-sociophobic <torikowashi@gmail.com>
2023-09-16 18:06:17 +0300

    add img conversion error handling

--- create-pngs.py
+++ create-pngs.py
@@ -13,8 +13,11 @@ for (path) in mypath:
 
 for file in f:
   if file[0].endswith('.dds'):
-    new_filename = file[0][0:-3] + 'png'
-    with image.Image(filename = file[1] + '/' + file[0]) as img:
-      img.compression = "no"
-      img.save(filename = file[1] + '/' + new_filename)
-    print(file)
+    try:
+      new_filename = file[0][0:-3] + 'png'
+      with image.Image(filename = file[1] + '/' + file[0]) as img:
+        img.compression = "no"
+        img.save(filename = file[1] + '/' + new_filename)
+      print(file)
+    except Exception:
+      print('failed ' + file[0])
